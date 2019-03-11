8bdc7f15065dc671b5ab39611440a267092def0f
kse1920 <44381451+kse1920@users.noreply.github.com>
2019-03-11 19:36:57 +0300

    Update AppBean.java
    test2 commit

--- src/main/java/ru/apache_maven/AppBean.java
+++ src/main/java/ru/apache_maven/AppBean.java
@@ -7,6 +7,6 @@ public class AppBean
         this.text = text;
     } 
     public String sayHello() {
-        return "Hello, testtest blablbalba" + text;
+        return "Hello, testtest blablbalbtftsrat sdsa" + text;
     } 
 }
