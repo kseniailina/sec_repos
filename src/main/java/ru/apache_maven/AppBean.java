b046d72af5568b503a4d99193e720bd98c9bcec5
kse1920 <44381451+kse1920@users.noreply.github.com>
2019-03-11 19:42:09 +0300

    Update AppBean.java
    change branches

--- src/main/java/ru/apache_maven/AppBean.java
+++ src/main/java/ru/apache_maven/AppBean.java
@@ -7,6 +7,6 @@ public class AppBean
         this.text = text;
     } 
     public String sayHello() {
-        return "Hello, testtest blablbalbtftsrat sdsa" + text;
+        return "Hello, testtest blablbalbt" + text;
     } 
 }
