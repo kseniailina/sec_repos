00900eb5b91285f4722d18ca06239804f45749be
kse1920 <44381451+kse1920@users.noreply.github.com>
2019-03-21 19:02:20 +0300

    Update AppTest.java

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -38,6 +38,6 @@ public class AppTest extends TestCase
 
     public void testAssert() {
 		 String message = new AppBean("Maven 2").sayHello();
-         Assert.assertEquals("Test Hello Machine", "Hello, Maven 2", message);
+         Assert.assertEquals("Test Hello Machine", "Hello Maven 2", message);
     }
 }
