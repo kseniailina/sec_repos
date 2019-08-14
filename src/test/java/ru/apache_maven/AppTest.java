ca120e0c5aceff7aaa6651581880285fa34589e1
kse1920 <kse1920@yandex.ru>
2019-08-14 15:11:36 +0300

    test

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -55,4 +55,9 @@ public class AppTest extends TestCase
 	String message3 = "Test Sec3";
         Assert.assertEquals("Second Test", "Test Sec", message3);
     }
+    
+     public void testAssert3() {
+	String message3 = "Test Sec3";
+        Assert.assertEquals("Second Test", "Test Sec", message3);
+    }
 }
