919cfda617a022161e18f926af8248a90549842e
kseniailina <kse1920@yandex.ru>
2019-09-09 18:53:40 +0300

    problem

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -135,6 +135,6 @@ public class AppTest extends TestCase
     }
     public void testAssert23() {
     String message23 = "Test Sec23";
-    Assert.assertEquals("Second Test", "Test Sec23", message23dhd);
+    Assert.assertEquals("Second Test", "Test Sec23", message23dhdgsfs);
     }
 }
