431dbbee6f8468eed9dc1d005bd66e653d2987c5
kseniailina <kse1920@yandex.ru>
2019-09-13 17:29:28 +0300

    fix

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -141,8 +141,4 @@ public class AppTest extends TestCase
         String message24 = "Test Sec24";
         Assert.assertEquals("Second Test", "Test Sec23", message24);
     }
-    public void testAssert25() {
-        String message25 = "Test Sec25";
-        Assert.assertEquals("Second Test", "Test Sec23", message25);
-    }
 }
