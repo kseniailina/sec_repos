93bef25ca09e2f36d2e8a87aa2d77839e236b34f
kseniailina <kse1920@yandex.ru>
2019-09-16 16:35:09 +0300

    sas

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -143,6 +143,6 @@ public class AppTest extends TestCase
     }
     public void testAssert25() {
         String message25 = "Test Sec25";
-        Assert.assertEquals("Second Test", "Test Sec25", message2);
+        Assert.assertEquals("Second Test", "Test Sec25", message25);
     }
 }
