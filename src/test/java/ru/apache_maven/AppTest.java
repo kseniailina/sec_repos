16dbe8787970a3ab25b5d4804be55c185677fa6c
kseniailina <50660900+kseniailina@users.noreply.github.com>
2020-04-10 18:58:19 +0300

    Update AppTest.java

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -67,12 +67,12 @@ public class AppTest extends TestCase
     }
     public void testAssert7() {
         String message7 = "Test Sec7";
-        Assert.assertEquals("Second Test", "Test Sec7", message7);
+        Assert.assertEquals("Second Test", "Test Sec71", message7);
     }
 
     public void testAssert8() {
         String message8 = "Test Sec8";
-        Assert.assertEquals("Second Test", "Test Sec8", message8);
+        Assert.assertEquals("Second Test", "Test Sec81", message8);
     }
 
     public void testAssert9() {
